b909489401ab4657c3c1ea9c262fe93c6f4fbc8b
Mathias Kaufmann <me@stei.gr>
2018-03-06 22:26:48 +0100

    Just run once if ONESHOT variable is set
    Signed-off-by: Mathias Kaufmann <me@stei.gr>

--- entrypoint.sh
+++ entrypoint.sh
@@ -5,11 +5,19 @@ set -x
 
 [ ! -d /configmap ] && echo "Sysctl configuration directory /configmap not found" && exit 1
 
-while true; do
+apply_configmap() {
     for option in $(ls -1 /configmap); do
-        value="$(cat /configmap/${option})"
+        value=$(cat /configmap/${option})
         sysctl -w "$option=$value"
     done
-    # TODO dump current sysctl state? (sysctl -a)
-    inotifywait -qq -e modify -e create -e delete /configmap/
-done
+}
+
+apply_configmap
+
+if [ -z "${ONESHOT:+x}" ]; then
+    while true; do
+        # TODO dump current sysctl state? (sysctl -a)
+        inotifywait -qq -e modify -e create -e delete /configmap/
+        apply_configmap
+    done
+fi
